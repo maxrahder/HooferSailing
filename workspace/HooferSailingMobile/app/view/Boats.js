81e7dd8821d87927cc8d4e96e4169785323a6f8d
towneley <adduah@gmail.com>
2013-09-16 13:18:40 -0500

    1:00 Mon.

---
+++ workspace/HooferSailingMobile/app/view/Boats.js
@@ -0,0 +1,22 @@
+Ext.define('HooferSailingMobile.view.Boats', {
+	extend: 'Ext.navigation.View',
+	xtype: 'boats',
+	
+	items: [{
+		store: {
+	        fields: ['name', 'age'],
+	        data: [
+	            {name: 'Jamie Avins',  age: 100},
+	            {name: 'Rob Dougan',   age: 21},
+	            {name: 'Tommy Maintz', age: 24},
+	            {name: 'Jacky Nguyen', age: 24},
+	            {name: 'Ed Spencer',   age: 26}
+	        ]
+    	},
+		//fullscreen: true,
+		//title: 'Boats View',
+        //html: 'This is a dataview within the navigationview. The list of boat categories, drawn from boats.json, goes here',
+		xtype: 'dataview',
+		itemTpl: '{name} is {age} years old'
+	}]
+});
--- workspace/HooferSailingMobile/app/view/Main.js
+++ workspace/HooferSailingMobile/app/view/Main.js
@@ -3,7 +3,8 @@ Ext.define('HooferSailingMobile.view.Main', {
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.Video',
+        'HooferSailingMobile.view.Boats'
     ],
     config: {
         tabBarPosition: 'bottom',
@@ -28,22 +29,12 @@ Ext.define('HooferSailingMobile.view.Main', {
                     "and refresh to change what's rendered here."
                 ].join("")
             },
+
+            
             {
                 title: 'Boats',
                 iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
+                xtype: 'boats'
             }
         ]
     }
