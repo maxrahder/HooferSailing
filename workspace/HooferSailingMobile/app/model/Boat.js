ae4a37f3beefeb030d1293b8f9e9af0745e6b4b1
towneley <adduah@gmail.com>
2013-09-17 09:53:14 -0500

    Changed boat field names

--- workspace/HooferSailingMobile/app/model/Boat.js
+++ workspace/HooferSailingMobile/app/model/Boat.js
@@ -2,9 +2,9 @@ Ext.define('User', {
     extend: 'Ext.data.Model',
     config: {
         fields: [
-            { name: 'boatId', type: 'int' },
-            { name: 'boatName', type: 'string' },
-            { name: 'boatHull', type: 'string' },
+            { name: 'name', type: 'string' },
+            { name: 'hull', type: 'string' },
+            { name: 'loan' },
         ],
         belongsTo: 'Fleet'
     }
