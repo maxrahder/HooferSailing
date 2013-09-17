dd61e6babe9e946f0216010d334c52c1479b9136
Max Rahder <max@sencha.com>
2013-09-17 12:52:15 -0500

    Added boats drill-down

--- workspace/HooferSailingMobile/app/view/Boats.js
+++ workspace/HooferSailingMobile/app/view/Boats.js
@@ -8,27 +8,20 @@ Ext.define('HooferSailingMobile.view.Boats', {
 				title: 'Boats',
 				xtype: 'dataview',
 				store: 'Fleets',
-				itemTpl: '{name}<hr/>'
+				itemTpl: '{name}<hr/>',
+				listeners: {
+					itemtap: function(dataview, index, target, record){
+						var navigationView = dataview.up('navigationview');
+						var boats = record.boats();
+						navigationView.push({
+							xtype: 'dataview',
+							title: 'Availability',
+							store: boats,
+							itemTpl: '{hull}'
+						});
+					}
+				}
 			}
-			/*
-                // Configures the detail views. 
-                detailCard: {  
-                    xtype: 'panel',
-                    scrollable: true,
-                    // "Uses styleHtmlContent to make the text look good."
-                    styleHtmlContent: true
-                },
-
-                // Event handler. Pushes a detail view.
-                // 'content' is the text (from the JSONP) that will appear as HTML in the detail panel
-                listeners: { 
-                    itemtap: function(nestedList, list, index, element, post) {
-                        this.getDetailCard().setHtml(post.get('content'));
-                    }
-                }
-            */
-
-            
 		]
 	}
 });
