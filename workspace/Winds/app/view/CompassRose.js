82fdb1a7af5d4280b7eaac9092c0f2cf386468c0
Max Rahder <max@sencha.com>
2013-09-17 09:33:59 -0500

    Working on compass

--- workspace/Winds/app/view/CompassRose.js
+++ workspace/Winds/app/view/CompassRose.js
@@ -5,13 +5,16 @@ Ext.define('Winds.view.CompassRose', {
 	tbar: [{
 		xtype: 'slider',
 		fieldLabel: 'Rotate',
+		labelWidth: 40,
 		width: 200,
 		minValue: 0,
 		maxValue: 360,
-		value: 315,
+		increment: 2,
+		value: 0,
+		name: 'rotate',
 		listeners: {
 			change: function(slider, value) {
-				var draw = slider.up('compassrose draw');
+				var draw = slider.up('compassrose').down('draw');
 				var sprite = draw.surface.items.first();
 				sprite.setAttributes({
 					rotation: {
@@ -24,13 +27,14 @@ Ext.define('Winds.view.CompassRose', {
 	layout: 'fit',
 	items: [{
 		xtype: 'draw',
+		viewBox: true,
 		items: [{
 			type: 'path',
 			path: 'M,10,675.32795,C,10,673.57572,270.18681,15.33843,270.61787,16.000147,C,272.06416,18.220368,530.86061,675.49217,530.38444,675.73581,C,530.07343,675.89493,471.43429,643.97006,400.07526,604.79165,L,270.33156,533.55814,L,140.79871,604.77908,C,69.555639,643.95057,10.981041,675.99999,10.63293,675.99999,C,10.284823,675.99999,10.000007,675.69756,10.000007,675.32795,L,10,675.32795,z',
 			fill: '#aa0000',
 			scale: {
-				x: .5,
-				y: .5,
+				x: .15,
+				y: .15,
 				centerX: 100,
 				centerY: 100
 			},
