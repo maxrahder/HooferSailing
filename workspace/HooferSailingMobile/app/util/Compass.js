d5cd3e3e88c978f626573a45c0a263af24ac90f9
Max Rahder <max@sencha.com>
2013-09-21 10:13:05 -0500

    Compass wind directions were midding one element

--- workspace/HooferSailingMobile/app/util/Compass.js
+++ workspace/HooferSailingMobile/app/util/Compass.js
@@ -5,17 +5,22 @@ Ext.define('HooferSailingMobile.util.Compass', {
 		[33.75, 'NNE'],
 		[56.25, 'NE'],
 		[78.75, 'ENE'],
+
 		[101.25, 'E'],
 		[123.75, 'ESE'],
 		[146.25, 'SE'],
 		[168.75, 'SSE'],
+
 		[191.25, 'S'],
 		[213.75, 'SSW'],
 		[236.25, 'SW'],
+		[258.75, 'WSW'],
+
 		[258.75, 'W'],
 		[303.75, 'WNW'],
 		[326.25, 'NW'],
 		[348.75, 'NNW'],
+
 		[360.0, 'N'],
 	],
 	degreesToRose: function(degrees) {
