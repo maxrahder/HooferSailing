805834df9b648babdebf84ec2e62519a7337d697
Max Rahder <max@rahder.com>
2014-04-08 15:43:45 -0500

    Tweaked refresh interval

--- workspace/hoofersailinginitial/app/controller/Refresh.js
+++ workspace/hoofersailinginitial/app/controller/Refresh.js
@@ -2,35 +2,50 @@ Ext.define('HooferSailingMobile.controller.Refresh', {
     extend: 'Ext.app.Controller',
     config: {
         stores: ['Fleets', 'Winds'],
-        models: ['Flag'], 
+        models: ['Flag'],
         refs: {},
         control: {},
-        autoRefreshEnabled: false,
+
+        interval: 0,
+        intervalId: -1, // Used to make sure we don't refresh using an old timer
+
+        autoRefreshEnabled: false
     },
-    
+
     init: function() {
         var me = this;
-        me.setAutoRefreshEnabled(true); 
+        me.setInterval(5 * 60 * 1000); // 5 minutes
     },
-   
+
+
+    applyInterval: function(interval) {
+        if (interval > 5000) {
+            return interval;
+        }
+    },
+    updateInterval: function(interval) {
+        this.setIntervalId(new Date().getTime());
+        this.doAutoRefresh(this.getIntervalId());
+    },
+
+
     updateAutoRefreshEnabled: function(newValue, oldValue) {
         this.doAutoRefresh();
     },
 
-    doAutoRefresh: function() {
+    doAutoRefresh: function(intervalId) {
         var me = this;
-        function recursiveRefresh() {
-            if (me.getAutoRefreshEnabled()) {
-                me.refresh();
-                Ext.defer(recursiveRefresh, 300000, me); // 5 minutes
-            }
+        if (intervalId !== this.getIntervalId()) {
+            return;
         }
-        recursiveRefresh();
+        me.refresh();
+        Ext.defer(me.doAutoRefresh, me.getInterval(), me, intervalId);
     },
 
     refresh: function() {
         Ext.getStore('Winds').fetch();
         //Ext.getStore('Fleets').load();
+        Ext.getStore('Fleets').loadUsingAdapter();
         HooferSailingMobile.model.Flag.load();
     },
 
