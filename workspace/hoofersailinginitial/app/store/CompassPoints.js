af960315343ed9655a78eb80dceaba17c2f50a0e
Max Rahder <max@rahder.com>
2014-04-26 17:47:40 -0500

    First production build -- buggy though

--- workspace/hoofersailinginitial/app/store/CompassPoints.js
+++ workspace/hoofersailinginitial/app/store/CompassPoints.js
@@ -2,85 +2,85 @@ Ext.define('HooferSailingMobile.store.CompassPoints', {
 	extend: 'Ext.data.Store',
 	requires: ['HooferSailingMobile.util.Compass'],
 	config: {
-	      fields: ['direction', 'frequency', 'averageKnots'],
-	      data: [
+		fields: ['direction', 'frequency', 'averageKnots'],
+		data: [
 
-	        {
-	          'direction': 'E',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'ESE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'SE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'SSE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        },
+			{
+				'direction': 'E',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'ESE',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'SE',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'SSE',
+				'frequency': 0,
+				'averageKnots': 0
+			},
 
-	        {
-	          'direction': 'S',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'SSW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'SW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'WSW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        },
+			{
+				'direction': 'S',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'SSW',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'SW',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'WSW',
+				'frequency': 0,
+				'averageKnots': 0
+			},
 
-	        {
-	          'direction': 'W',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'WNW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'NW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'NNW',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        },
+			{
+				'direction': 'W',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'WNW',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'NW',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'NNW',
+				'frequency': 0,
+				'averageKnots': 0
+			},
 
-	        {
-	          'direction': 'N',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'NNE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'NE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }, {
-	          'direction': 'ENE',
-	          'frequency': 0,
-	          'averageKnots': 0
-	        }
-      	]
+			{
+				'direction': 'N',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'NNE',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'NE',
+				'frequency': 0,
+				'averageKnots': 0
+			}, {
+				'direction': 'ENE',
+				'frequency': 0,
+				'averageKnots': 0
+			}
+		]
 	},
 
-	updateDataUsingWinds: function(windsStore){
-		
+	updateDataUsingWinds: function(windsStore) {
+
 		var me = this;
 		var groups = windsStore.getGroups();
 
@@ -95,7 +95,7 @@ Ext.define('HooferSailingMobile.store.CompassPoints', {
 		//*/
 
 
-		Ext.Array.forEach(groups, function(group){
+		Ext.Array.forEach(groups, function(group) {
 			var direction = group.name;
 			var frequency = group.children.length;
 
@@ -105,7 +105,12 @@ Ext.define('HooferSailingMobile.store.CompassPoints', {
 
 			var index = me.findExact('direction', direction);
 			var r = me.getAt(index);
-			r.set({frequency: frequency, averageKnots: (averageKnots / 30) * radiusUnit});
+			if (r) {
+				r.set({
+					frequency: frequency,
+					averageKnots: (averageKnots / 30) * radiusUnit
+				});
+			}
 			//r.set({frequency: frequency, averageKnots: averageKnots});
 		});
 	}
