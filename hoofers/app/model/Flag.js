401f023deaa75b4154cafa7fbdb3aa4c9d35b1b4
Max Rahder <max@sencha.com>
2017-07-01 18:49:01 -0500

    Changed flag to use  url: 'http://ehs.wisc.edu/current-flag.php' since http://testout.hoofersailing.org/feed.php appears to be gone.

--- hoofers/app/model/Flag.js
+++ hoofers/app/model/Flag.js
@@ -13,8 +13,8 @@ Ext.define('Hoofers.model.Flag', {
     load: function() {
         var me = this;
         Ext.data.JsonP.request({
-            // url: 'http://ehs.wisc.edu/current-flag.php',
-            url: 'http://testout.hoofersailing.org/feed.php',
+            url: 'http://ehs.wisc.edu/current-flag.php',
+            // url: 'http://testout.hoofersailing.org/feed.php',
             success: function(result, request) {
                 var updated = {}; //Ext.Date.format(result.updated, 'Y-m-d H:i:s');
                 me.update(result.color, updated);
